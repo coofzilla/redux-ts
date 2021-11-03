574fbdc3694100d38b9e581ce00a5e693d735b71
Jay Hernandez <jeric614@yahoo.com>
2021-11-03 20:55:32 +0900

    initial commit f/redux w/ts fundamentals

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,15 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+};
+
+export default reducer;
