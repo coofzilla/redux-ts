3f163e5b2ed5b7e3454161fc9616e0aeda5985c7
Jay Hernandez <jeric614@yahoo.com>
2021-11-08 16:25:49 +0900

    configured stored

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
@@ -26,10 +27,10 @@ export const searchRepositories = (term: string) => {
         type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
         payload: names,
       });
-    } catch (err) {
+    } catch (error: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
-        payload: err.message,
+        payload: error.message,
       });
     }
   };
