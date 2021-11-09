b278a59331f22a6f7bff680ace6f32d336690885
Jay Hernandez <jeric614@yahoo.com>
2021-11-09 10:32:05 +0900

    bound/called  action creators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,23 @@
+import React, { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    searchRepositories(term);
+    //refactored into useAction
+    // dispatch(actionCreators.searchRepositories(term));
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      {/* hover over to get type hint */}
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
