a00e958e5a7d6962808130a1527ac2b8d3c250a2
Jay Hernandez <jeric614@yahoo.com>
2021-11-09 15:08:26 +0900

    implemented conditional rendering f/error/loading

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -10,7 +10,6 @@ const RepositoriesList: React.FC = () => {
     (state) => state.repositories
   );
 
-
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -26,6 +25,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
